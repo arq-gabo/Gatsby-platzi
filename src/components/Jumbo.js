4a58d787b5567a533c7ae97c69f90baa3bbfd597
arq-gabo <jgabrielguerral@gmail.com>
2020-10-26 18:32:07 -0400

    [FEATURE]Usando React para manejar la parte visual e interactiva de nuestra aplicación

---
+++ src/components/Jumbo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyledJumbo } from "../styles/components";
+
+export default function Jumbo() {
+    return (
+        <StyledJumbo>
+            <div>
+                <h2>!Consigue el mejor swap exclisivo y especial de Gabo</h2>
+                <small>Piezas elegantes para los mejores</small>
+            </div>
+        </StyledJumbo>
+    )
+}
