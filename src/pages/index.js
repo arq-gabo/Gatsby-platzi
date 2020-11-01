2bbac1b2b44ace9a78b413cb677b9101a9ac4de7
arq-gabo <jgabrielguerral@gmail.com>
2020-11-01 17:15:33 -0400

    [FEATURE]Productos en React

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { Jumbo } from "../components"
-import styled from "styled-components"
-import { SEO } from "../components"
+import { Jumbo, SEO, Products } from "../components"
 
 export const query = graphql`
-query GET_DESCRIPTION{
+query GET_DATA{
   allSite{
     edges{
       node{
@@ -15,27 +13,36 @@ query GET_DESCRIPTION{
       }
     }
   }
+
+  allStripePrice{
+    edges{
+      node{
+        id
+        unit_amount
+        product{
+          name
+          metadata{
+            description
+            wear
+            img
+          }
+        }
+      }
+    }
+  }
 }
 `
-const Button = styled.button`
-  width: 8rem;
-  background-color: #98ca3f;
-  border: none;
-  border-radius: 10px;
-  color: ${props => props.color};
-  &:hover{
-    transform: scale(1.4)
-  }
-`
+const IndexPage = ({ data }) =>{
 
+  console.log(data)
 
-const IndexPage = ({ data }) =>  
- (
+  return (
   <>
     <SEO title="Home" />
     <Jumbo description={data.allSite.edges[0].node.siteMetadata.description} />
-    <Button color="gray"> Comprar </Button>
+    <Products products={data.allStripePrice.edges} />
   </>
 )
+}
 
 export default IndexPage
